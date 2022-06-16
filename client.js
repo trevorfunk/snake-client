1fb5a15adf3c3bb3fb76e1ff586de09573f563a8
trevorfunk <trevorjonfunk@gmail.com>
2022-06-16 10:55:08 -0700

    add movement keys

--- client.js
+++ client.js
@@ -1,25 +1,25 @@
 const net = require("net");
 
-const connect = function () {
- const conn = net.createConnection({
-  port: 50541,// PORT number here,
-  host: "localhost", // IP address here,
- });
+const connect = function() {
+  const conn = net.createConnection({
+    port: 50541,// PORT number here,
+    host: "localhost", // IP address here,
+  });
 
- conn.on('connect', () => {
-  console.log("Successfully connected to a game server");
-  conn.write("Name: TRE");
- });
+  conn.on('connect', () => {
+    console.log("Successfully connected to a game server");
+    conn.write("Name: TRE");
+  });
 
   
- conn.on('data', () => {
-  console.log('you ded cuz you idled') //Message for connection lost 
- })
+  conn.on('data', () => {
+    console.log('you ded cuz you idled'); //Message for connection lost
+  });
  
- // interpret incoming data as text
- conn.setEncoding("utf8");
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
 
- return conn;
+  return conn;
 };
 
 module.exports = {connect};
--- input.js
+++ input.js
@@ -1,16 +1,36 @@
-const handleUserInput = function (key) { //add key to terminate game
- if (key === '\u0003') {
-  process.exit();
- }
+const net = require("net");
+
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function(key) { //add key to terminate game
+    if (key === '\u0003') {
+      process.exit();
+    }
+    if (key === 'w') {
+      conn.write("Move: up");
+    }
+    if (key === 'a') {
+      conn.write("Move: left");
+    }
+    if (key === 's') {
+      conn.write("Move: down");
+    }
+    if (key === 'd') {
+      conn.write("Move: right");
+    }
+  };
+
+  stdin.on("data", handleUserInput);
+  return stdin;
 };
 
-const setupInput = function () {
- const stdin = process.stdin;
- stdin.setRawMode(true);
- stdin.setEncoding("utf8");
- stdin.resume();
- stdin.on("data", handleUserInput);
- return stdin;
-};
 
-module.exports = {setupInput}
+
+module.exports = {setupInput};
